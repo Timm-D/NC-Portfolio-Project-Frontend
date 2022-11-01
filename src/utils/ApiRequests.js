8dc51776057e10b7e6630e5ddfab5b864dc9c45e
timm-d <timdunstan1@gmail.com>
2022-11-01 10:51:22 +0000

    added path to get all reviews

--- src/utils/ApiRequests.js
+++ src/utils/ApiRequests.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
 const baseURL = axios.create({
-    baseURL: "https://tims-board-games-api.herokuapp.com/api"
+  baseURL: "https://tims-board-games-api.herokuapp.com/api",
 });
+
+export const getAllReviews = (reviews) => {
+  return baseURL.get(`/all_reviews/${reviews}`).then((res) => {
+    return res.data;
+  });
+};
