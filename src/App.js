c51d1df7d3d78c7deeb04e5aaa12034be8d58dca
timm-d <timdunstan1@gmail.com>
2022-11-01 11:16:01 +0000

    outline reviewList component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Header from "./Components/Header"
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import ReviewList from "./Components/ReviewList";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-    <Header/>
-
-    </div>
+      <div className="App">
+        <Header />
+        <ReviewList/>
+        <Routes>
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/Components/ReviewList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const ReviewList = (props) => {
+    console.log(props)
+    return <p>hello</p>
+}
+
+export default ReviewList
